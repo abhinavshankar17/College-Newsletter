08473013bca7420244080497c192b40cf28b99a2
abhinavshankar17 <abhinav.shankar17@gmail.com>
2025-08-22 01:28:58 +0530

    fixed-errors

--- routes/cyberSecurity.js
+++ routes/cyberSecurity.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { getCyberSecurity, addCyberSecurity } from "../controllers/cyberSecurity.js";
 const router = express.Router();
-// GET - show page
-router.get("/cyber", getCyberSecurity);
-// POST - add new doc
-router.post("/cyber/add", addCyberSecurity);
-export default router;
 
+// GET - show page
+router.get("/", getCyberSecurity);
+
+// POST - add new doc
+router.post("/add", addCyberSecurity);
+
+export default router;
