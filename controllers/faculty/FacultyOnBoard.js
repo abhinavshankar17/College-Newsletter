76a4ca973e60c4073a4c050ac9d24b1fc6971173
abhinavshankar17 <abhinav.shankar17@gmail.com>
2025-09-17 02:10:16 +0530

    fixing errors

--- controllers/faculty/FacultyOnBoard.js
+++ controllers/faculty/FacultyOnBoard.js
@@ -1,13 +1,14 @@
 import FacultyOnBoard from "../../models/faculty/FacultyOnBoard.js";
 export const getAllFacultyOnBoard = async (req, res) => {
-    try {
-        const faculty = await FacultyOnBoard.findOne();
-        res.render("faculty/FacultyOnBoard", {faculty});
-    } catch (error) {
-        console.error("Error fetching faculty on board:", error);
-        res.status(500).render("error", { message: "Server Error" });
-    }
+  try {
+    const faculty = await FacultyOnBoard.find();  // <-- get all docs
+    res.render("faculty/FacultyOnBoard", { faculty });
+  } catch (error) {
+    console.error("Error fetching faculty on board:", error);
+    res.status(500).render("error", { message: "Server Error" });
+  }
 };
+
 
 // Add new faculty on board (CRUD - Create)
 export const addFacultyOnBoard = async (req, res) => {
