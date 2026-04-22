f8497970d2c294ae4702478d86bfb2dfb757aab3
abhinavshankar17 <abhinav.shankar17@gmail.com>
2026-04-22 12:22:24 +0530

    render updates

--- server.js
+++ server.js
@@ -112,6 +112,7 @@ app.get("/students/placement-highlights", (req, res) => {
 });
 
 // Start server
-app.listen(3000, () => {
-  console.log("🚀 Server running at http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
